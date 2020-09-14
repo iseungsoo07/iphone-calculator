28963915a9f69082b8c17d13b6c689111f8e5a24
Seungsoo <43941337+iseungsoo07@users.noreply.github.com>
2020-09-14 19:00:21 +0900

    Create eqaul function

--- index.js
+++ index.js
@@ -1,17 +1,110 @@
-let result = document.querySelector(".js-result");
-let reset = document.querySelector(".js-reset");
-let numbers = Array.from(document.querySelectorAll(".js-number"));
+const result = document.querySelector(".js-result");
+const reset = document.querySelector(".js-reset");
+const equals = document.querySelector(".js-equals");
+const dot = document.querySelector(".js-dot");
+const plusMinusBtn = document.querySelector(".js-plusMinus");
+const operators = Array.from(document.querySelectorAll(".js-operators"));
+const numbers = Array.from(document.querySelectorAll(".js-number"));
 let firstInput = "",
-   secondInput = "";
-let isFirst = false;
+   secondInput = "",
+   initializeInput = "";
+
+let firstClick = true;
+let clickedOperator = "";
+let positive = true;
+
+let isFirst = true;
+let answer = 0;
+
+function checkIsFirst() {
+   if (isFirst) {
+      result.value = initializeInput;
+      isFirst = false;
+   }
+}
 
 function handleNumberClick(event) {
-   console.log(event.target.innerText);
-   result.innerText += event.target.innerText;
+   let clickedNumber = event.target.innerText;
+   checkIsFirst();
+
+   if (clickedOperator === "") {
+      result.value += clickedNumber;
+      firstInput = result.value;
+      console.log(firstInput);
+   } else {
+      checkIsFirst();
+      result.value += clickedNumber;
+      secondInput = result.value;
+      console.log(secondInput);
+   }
+}
+
+function handleOperatorClick(event) {
+   isFirst = true;
+   console.log(clickedOperator);
+   if (clickedOperator === "") {
+      clickedOperator = event.target.innerText;
+   } else {
+   }
+}
+
+function handleEqaualClick() {
+   switch (clickedOperator) {
+      case "+":
+         answer = Number(firstInput) + Number(secondInput);
+         result.value = answer;
+         firstInput = answer;
+         break;
+      case "-":
+         answer = Number(firstInput) - Number(secondInput);
+         result.value = answer;
+         firstInput = answer;
+         break;
+      case "x":
+         answer = Number(firstInput) * Number(secondInput);
+         result.value = answer;
+         firstInput = answer;
+         break;
+      case "÷":
+         answer = Number(firstInput) / Number(secondInput);
+         result.value = answer;
+         firstInput = answer;
+         break;
+      default:
+         return;
+   }
+}
+
+function handlePlusMinusClick() {
+   if (positive) {
+      result.value = `-${result.value}`;
+
+      positive = false;
+   } else {
+      result.value = `${Math.abs(Number(result.value))}`;
+
+      positive = true;
+   }
+}
+
+function handleDotClick() {
+   if (isFirst === true) {
+      result.value = `0.`;
+      isFirst = false;
+   } else {
+      result.value = `${result.value}.`;
+   }
 }
 
 function handleResetClick() {
-   result.innerText = 0;
+   result.value = 0;
+   isFirst = true;
+   firstClick = true;
+   positive = true;
+   firstInput = "";
+   secondInput = "";
+   clickedOperator = "";
+   answer = 0;
 }
 
 function init() {
@@ -19,7 +112,14 @@ function init() {
       element.addEventListener("click", handleNumberClick);
    });
 
+   operators.forEach(function (element) {
+      element.addEventListener("click", handleOperatorClick);
+   });
+
+   equals.addEventListener("click", handleEqaualClick);
+   plusMinusBtn.addEventListener("click", handlePlusMinusClick);
    reset.addEventListener("click", handleResetClick);
+   dot.addEventListener("click", handleDotClick);
 }
 
 init();
